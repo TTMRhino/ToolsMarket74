739e5f81235451f4e83d112716c020ecb9e89595
Morozov Dmitriy Viktorovich <DMorozov@gksm.local>
2021-05-04 17:09:33 +0500

    04/05/21

--- modules/admin/views/layouts/admin.php
+++ modules/admin/views/layouts/admin.php
@@ -166,7 +166,7 @@ AdminAsset::register($this);
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="<?= \Yii::$app->homeUrl ?>" target="_blank" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
@@ -179,7 +179,10 @@ AdminAsset::register($this);
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <span class="" style="color:#a9a9a9;" ><?= \Yii::$app->user->identity->username ?></span>
+          <a href=" <?= Url::to(['auth/logout']) ?>"> : LogOut</a>
+          
+          
         </div>
       </div>
 
@@ -265,6 +268,7 @@ AdminAsset::register($this);
         <div class="row">
          
          <?= $content ?>
+         <?php debug(\Yii::$app->user->identity)?>
         </div>
         <!-- /.row -->
       </div><!-- /.container-fluid -->
